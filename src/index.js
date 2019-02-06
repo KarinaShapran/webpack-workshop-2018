40730550a89b10da105fc302988feebf00954990
Karina Shapran <karina.shapran@uptech.team>
2019-02-06 16:07:36 +0200

    031 all module files

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import nav from "./nav";
+import { top, bottom } from './footer';
+import makeButton from './button';
+import { makeColorStyle } from './button-style';
+
+console.log(
+  nav(),
+  top,
+  bottom,
+  makeButton("primary"),
+  makeColorStyle("cyan")
+);
