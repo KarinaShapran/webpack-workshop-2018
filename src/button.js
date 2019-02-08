7c5df398190d7e116e21d06abfcb0091fa05eb56
Karina Shapran <karina.shapran@uptech.team>
2019-02-08 14:58:26 +0200

    my changes

--- src/button.js
+++ src/button.js
@@ -3,8 +3,13 @@
  * @param {string} buttonName
  * @returns {string}
  */
-const makeButton = (buttonName) => {
-  return `Button ${buttonName}`;
+const makeButton = buttonName => {
+  const buttonLabel = `Button ${buttonName}`;
+
+  const button = document.createElement("button");
+  button.innerText = buttonLabel;
+
+  return button;
 };
 
 module.exports = makeButton;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-import nav from "./nav";
-import { top, bottom } from './footer';
+import nav from './nav';
+import { footer } from './footer';
 import makeButton from './button';
 import { makeColorStyle } from './button-style';
+import Foo from './foo.ts';
 
-console.log(
-  nav(),
-  top,
-  bottom,
-  makeButton("primary"),
-  makeColorStyle("cyan")
-);
+import imageUrl from './logo.jpg';
+import makeImage from './image';
+
+import css from './footer.css';
+import buttonStyle from './button.css';
+
+const button = makeButton('Yay!');
+button.style = makeColorStyle('cyan');
+
+const image = makeImage(imageUrl, 200, 200);
+
+document.body.appendChild(button);
+document.body.appendChild(footer);
+document.body.appendChild(image);
+
